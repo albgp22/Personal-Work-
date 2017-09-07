d3112a7fdb50b75ef33cca317b97155a67b11f7d
albgp22 <31456705+albgp22@users.noreply.github.com>
2017-09-07 15:17:57 +0200

    Update 10209.cpp

--- UVAOnlineJudgle/10209.cpp
+++ UVAOnlineJudgle/10209.cpp
@@ -2,11 +2,6 @@
 #include <math.h>
 #include <iomanip>
 
-
-double round3(double val){
-	return roundf(val * 1000) / 1000;
-}
-
 int main(){
 	double mu=0.02169426125474089; //integrate from 0 to 1/2 of 1-sqrt(1-u^2)
 	double PI=3.141592653589793;
@@ -16,9 +11,6 @@ int main(){
 		double C=8*T; //Area of all enclosed areas
 		double A=4*(a*a*((4-PI)/4)-4*T); //Area of pointed curved triangles
 		double B=a*a-A-C; //Remaining area (stripped area)
-		//A=round3(A);
-		//B=round3(B);
-		//C=round3(C);
 		std::cout << std::fixed << std::setprecision(3) << B << " " << A << " " << C << std::endl;
 	}
 }
